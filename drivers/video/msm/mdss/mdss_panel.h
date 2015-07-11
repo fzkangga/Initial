a5976673ecefe9a764ca22fac9c86ffb701ca8c7
dev-harsh1998 <harshit.india@linaro.org>
2015-07-11 05:49:30 -0700

    mdss: Kick live display driver!
    This reverts commit 93ee306147924e792e611dc054ddc3b9649cae45.
    
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -370,8 +370,6 @@ struct mdss_mdp_pp_tear_check {
 	u32 refx100;
 };
 
-struct mdss_livedisplay_ctx;
-
 struct mdss_panel_info {
 	u32 xres;
 	u32 yres;
@@ -447,8 +445,6 @@ struct mdss_panel_info {
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
-
-	struct mdss_livedisplay_ctx *livedisplay;
 };
 
 struct mdss_panel_data {
